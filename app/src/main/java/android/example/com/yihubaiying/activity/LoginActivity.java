637a75580331087b50ff2e0dc33481b6ce2c16a4
Thingvellir <Thingvellir@163.com>
2017-11-21 11:56:03 +0800

    完成工作哇咔咔

---
+++ app/src/main/java/android/example/com/yihubaiying/activity/LoginActivity.java
@@ -0,0 +1,41 @@
+package android.example.com.yihubaiying.activity;
+
+import android.content.Intent;
+import android.example.com.yihubaiying.MainActivity;
+import android.example.com.yihubaiying.activity.redvelet.BaseActivity;
+import android.example.com.yihubaiying.utils.AppManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.example.com.yihubaiying.R;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.amap.api.maps.model.Text;
+
+public class LoginActivity extends BaseActivity {
+    private Button button;
+    private TextView register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button= (Button) findViewById(R.id.de_login_sign);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+        register= (TextView) findViewById(R.id.de_login_register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+                AppManager.getAppManager().finishActivity();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/android/example/com/yihubaiying/activity/RegisterActivity.java
@@ -0,0 +1,26 @@
+package android.example.com.yihubaiying.activity;
+
+import android.content.Intent;
+import android.example.com.yihubaiying.MainActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.example.com.yihubaiying.R;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        button= (Button) findViewById(R.id.reg_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
